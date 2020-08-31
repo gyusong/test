55bb848f0f2d7ae5a8368b4447ac50dd703cb3e7
yus <yus@gzyztd.com>
2020-08-31 15:03:41 +0800

    。。

---
+++ testrepo/src/main/java/com/example/testrepo/ToastUtils.java
@@ -0,0 +1,12 @@
+package com.example.testrepo;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtils {
+
+    public static void toast(Context context,String content){
+
+        Toast.makeText(context,content,Toast.LENGTH_LONG).show();
+    }
+}
